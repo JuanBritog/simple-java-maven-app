ac3dc73b0535feeaa4d638819fdf21d5fda130e7
JuanBritog <83216119+JuanBritog@users.noreply.github.com>
2023-09-20 15:27:48 +0200

    Update javamavenDSL2.groovy
    Update javamavenDSL2.groovy

--- DSL/javamavenDSL2.groovy
+++ DSL/javamavenDSL2.groovy
@@ -1,9 +1,9 @@
 job('Java Maven App DSL 2') {
     description('Java Maven App con DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/simple-java-maven-app.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/JuanBritog/simple-java-maven-app.git', 'master') { node ->
+            node / gitConfigName('JuanBritog')
+            node / gitConfigEmail('brito.grandes@gmail.com')
         }
     }
     steps {
